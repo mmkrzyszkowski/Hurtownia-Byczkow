32615c4ef87134140126a01323cb2aa5c47efe8b
DESKTOP-T1FEPOR\Filip <filip.wiatrowski@gmail.com>
2019-05-04 18:40:26 +0200

    Index page

---
+++ src/main/java/com/hurtownia/HurtowniaByczkowApplication.java
@@ -0,0 +1,13 @@
+package com.hurtownia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HurtowniaByczkowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HurtowniaByczkowApplication.class, args);
+	}
+
+}
